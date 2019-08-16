79f4bf91792ca7b95d822302d5d5bcb0165a29d2
yurakobernik <yurii.k@maxitechs.com>
2019-08-16 17:35:54 +0300

    added menu

--- lib/pages/lake/lake.page.dart
+++ lib/pages/lake/lake.page.dart
@@ -1,59 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/user/services/user.model.dart';
 import '../../widgets/drawer.widget.dart';
 
-class LakePage extends StatefulWidget {
-  @override
-  _LakePageState createState() => _LakePageState();
-}
-
-class _LakePageState extends State<LakePage> {
-  bool _loading = false;
-  User _user;
-
+class LakePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('User'),
+        title: const Text('Lake'),
       ),
       drawer: DrawerMain(selected: "lake"),
-      body: SafeArea(
-        child: _loading ? _buildLoading() : _buildBody(),
+      body: ListView(
+        children: <Widget>[
+          Image.asset(
+            'images/lake.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          TitleWidget(),
+          ButtonsWidget(),
+          DescriptionWidget(),
+        ],
       ),
     );
   }
+}
 
-  Widget _buildBody() {
-    if (_user != null) {
-      return _buildContent();
-    } else {
-      return _buildInit();
+class TitleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Lake 1 description',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandera Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
     }
-  }
 
-  Widget _buildInit() {
-    return Center(
-      child: RaisedButton(
-        child: const Text('Load user data'),
-        onPressed: () {
-          setState(() {
-            _loading = true;
-          });
-        },
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
       ),
     );
   }
+}
 
-  Widget _buildContent() {
-    return Center(
-      child: Text('Hello ${_user.name} ${_user.surname}'),
-    );
-  }
-
-  Widget _buildLoading() {
-    return const Center(
-      child: CircularProgressIndicator(),
+class DescriptionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Text(
+        ''' 
+        Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
     );
   }
 }
